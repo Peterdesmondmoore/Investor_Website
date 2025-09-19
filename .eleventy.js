7706547ee651399fa8a351abc87cf1e3d1b387c8
Peterdesmondmoore <88955990+Peterdesmondmoore@users.noreply.github.com>
2025-09-19 21:30:49 +1000

    xxx

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,17 @@
+const { Liquid } = require('liquidjs');
+const liquidOptions = {
+    extname: '.liquid',
+    dynamicPartials: true,
+};
+
 module.exports = function(eleventyConfig) {
+  // === New additions for the 'safe' filter ===
+  eleventyConfig.setLiquidOptions(liquidOptions);
+  eleventyConfig.addFilter('safe', function(value) {
+    return value;
+  });
+  // ==========================================
+
   // Create a custom collection for all files in the "market-reports" folder
   eleventyConfig.addCollection("reports", function(collectionApi) {
     return collectionApi.getFilteredByGlob("market-reports/*.html");
@@ -7,6 +20,7 @@ module.exports = function(eleventyConfig) {
   // Passthrough any assets or folders you might need
   // eleventyConfig.addPassthroughCopy("css"); // Example
 
+  // Return your site's configuration
   return {
     dir: {
       input: ".", // Root directory is the input
